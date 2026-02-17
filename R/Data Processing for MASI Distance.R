452e522899188ca34ecff61a4bf3c879de259363
anonymousolive23 <leo.ghtan@gmail.com>
2026-02-17 15:48:44 +0000

    Add files via upload

---
+++ R/Data Processing for MASI Distance.R
@@ -0,0 +1,59 @@
+library(tidyr)
+library(dplyr)
+library(stats)
+library(psych)
+library(janitor)
+
+#setup
+setwd("C:/Users/ghtan/Downloads/Research Internship/Open Secrets Data/Intercoder Reliability")
+Coder_1 = read.csv("Leo_Code-Document_Analysis(Final).csv")
+Coder_2 = read.csv("MS_Random Coding Subset Combined(LeoModFinal).csv")
+Coder_3 = read.csv("SV_Random Coding Subset Combined(LeoModFinal).csv")
+
+#cleaning names
+names(Coder_1) <- gsub("\\.", "_", names(Coder_1))
+colnames(Coder_1) <- gsub("_$", "", colnames(Coder_1))
+
+names(Coder_2) <- gsub("\\.", "_", names(Coder_2))
+colnames(Coder_2) <- gsub("_$", "", colnames(Coder_2))
+
+names(Coder_3) <- gsub("\\.", "_", names(Coder_3))
+colnames(Coder_3) <- gsub("_$", "", colnames(Coder_3))
+
+#vectorise names
+all_cols = colnames(Coder_1) #extract the headers (codes) from the binary coding matrix
+
+#Create the new column by applying a function to each row
+Coder_1$Codes <- apply(Coder_1[, all_cols], 1, function(row) {
+  codes <- all_cols[which(row == 1)]
+  paste(codes, collapse = "|")
+})
+
+Coder_1 = Coder_1[c("ResponseID", "Codes")]
+
+Coder_2$Codes <- apply(Coder_2[, all_cols], 1, function(row) {
+  codes <- all_cols[which(row == 1)]
+  paste(codes, collapse = "|")
+})
+
+Coder_2 = Coder_2[c("ResponseID", "Codes")]
+
+Coder_3$Codes <- apply(Coder_3[, all_cols], 1, function(row) {
+  codes <- all_cols[which(row == 1)]
+  paste(codes, collapse = "|")
+})
+
+Coder_3 = Coder_3[c("ResponseID", "Codes")]
+
+#processing
+colnames(Coder_1) = c("ResponseID", "Coder_1")
+colnames(Coder_2) = c("ResponseID", "Coder_2")
+colnames(Coder_3) = c("ResponseID", "Coder_3")
+
+newdf = merge(Coder_1, Coder_2, by = "ResponseID")
+newdf = merge(newdf, Coder_3, by="ResponseID")
+
+newdf = newdf[-1]
+newdf = replace(newdf, newdf=="", "|")
+
+write.csv(newdf, "Combined Coding Matrix for MVAlpha.csv", row.names = F)
---
+++ R/MASI Distance MVNA.R
@@ -0,0 +1,62 @@
+library(irrCAC)
+library(stringr)
+
+#setup
+setwd("C:/Users/ghtan/Downloads/Research Internship/Open Secrets Data/Intercoder Reliability")
+getOption("max.print")
+options("max.print"=5000)
+
+#setup to use reticulate and miniconda
+reticulate::install_miniconda() #install miniconda. paste the directory output from R after installation is done in the line below for condaenv
+reticulate::use_miniconda(condaenv = "C:/Users/ghtan/AppData/Local/r-miniconda", required = TRUE) #force R to use miniconda
+reticulate::py_config() #check that this went correctly - great!
+
+library(reticulate) #import full list of reticulate functions. IMPORTANT: DO NOT DO THIS BEFORE LINES ABOVE OR R WILL USE THE DEFAULT PYTHON INSTALL ON YOUR BASE MACHINE; IF THERE IS NO DEFAULT PYTHON INSTALL THEN IT WILL FAIL
+
+#write a function to calculate MASI distance (see:https://www.cs.columbia.edu/nlp/papers/2006/passonneau_06.pdf)
+pybuiltins = import_builtins() #import built-in functions from python
+py_install("nltk") #download natural language processing toolkit (NLTK) module to miniconda
+nltk = import("nltk") #import NLTK as an R module
+masi = function(x,y,split = " ") 
+{
+  if(length(str_split(x,split,simplify = F)[[1]]) == 1) 
+  {
+    x = str_split(x,split,simplify = F)
+  }else
+  {
+    x = unlist(str_split(x,split,simplify = F)) 
+  }
+  
+  if(length(str_split(y,split,simplify = F)[[1]]) == 1) 
+  {
+    y = str_split(y,split,simplify = F) 
+  }else
+  {
+    y = unlist(str_split(y,split,simplify = F)) 
+  }
+  
+  masiD =  nltk$masi_distance(pybuiltins$set(x),pybuiltins$set(y)) #calculate MASI distance using nltk built-in functions
+  masiD  #output MASI distance
+}
+
+##############################################################################################################################
+
+merge = read.csv("Combined Coding Matrix for MVAlpha.csv") #import ALL codes, preprocessed to contain coders by column and units by row
+
+#calculating weight matrix for all observed combination of labels using MASI distance
+labelsCombAll = unique(unlist(merge)) #create a vector of all the unique labels (codes) in the dataset we imported
+distMasi = matrix(nrow = length(labelsCombAll),ncol = length(labelsCombAll), #create an empty matrix with all the combinations of codes present in the data for MASI distances
+                   dimnames = list(labelsCombAll,labelsCombAll))
+
+for(i in 1:nrow(distMasi)) #over all the permutations in the matrix, calculate MASI distances
+  for(j in 1:ncol(distMasi))
+  {
+    distMasi[i,j] = 1-masi(labelsCombAll[i],labelsCombAll[j],"|")
+  }
+
+mv_alpha = krippen.alpha.raw(merge, weights = distMasi,categ.labels = rownames(distMasi)) #use the MASI distances as weights to calculate kripp's a for the full dataset
+mv_alpha$est
+
+##############################################################################################################################
+save.image(file = "mvalpha.RData")
+load(file = "mvalpha.RData")
